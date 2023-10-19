d625163295fa971297f45c057112c33332e1a760
ankeet779 <100555111+ankeet779@users.noreply.github.com>
2023-10-19 14:19:14 +0545

    added student calender

--- unify/frontend/src/app.jsx
+++ unify/frontend/src/app.jsx
@@ -1,10 +1,12 @@
-import SignUp from './components/signup'
+// import SignUp from './components/signup'
+import Calender from './components/Student/Calender'
 import { ChakraProvider } from '@chakra-ui/react'
 export function App() {
 
   return (
     <ChakraProvider>
-      <SignUp />
+      {/* <SignUp /> */}
+      <Calender/>
     </ChakraProvider>
 
   )
