0ec0e2e11e4e8fed6d3924935d8e6ee55d10c42e
Shishir Tamrakar <stamrakar545@gmail.com>
2023-10-22 19:52:32 +0545

    Added navbar

--- unify/frontend/src/app.jsx
+++ unify/frontend/src/app.jsx
@@ -1,15 +1,25 @@
 // import SignUp from './components/signup'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import NavBar from './components/Navbar/Navbar';
+import Home from './components/Home';
 import Events from './components/Student/Events'
 import SignUp from './components/signup'
 import { ChakraProvider } from '@chakra-ui/react'
-export function App() {
 
+export function App() {
+  const currentUrl = window.location.pathname;
   return (
     <ChakraProvider>
-      {/* <SignUp /> */}
-      {/* <Calender/> */}
-      <Events/>    
-</ChakraProvider>
-
+      {currentUrl !== '/signup' && <NavBar />}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route exact path="/signup" Component={SignUp} />
+          <Route exact path="/events" Component={Events} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   )
 }
