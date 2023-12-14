48716fa09d2d8729185d13872386f27ab1dc0b7b
ankeet779 <100555111+ankeet779@users.noreply.github.com>
2023-12-14 19:24:03 +0545

    Events for student

---
+++ unify/frontend/src/components/Student/StudentEvents.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ViewEvents from "../Events/ViewEvents";
+
+const StudentEvent = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <ViewEvents />
+    </div>
+  );
+};
+
+export default StudentEvent;
