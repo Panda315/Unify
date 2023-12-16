4cc624940f6c5ed4aa7cb00893e27621efc318d5
ankeet779 <100555111+ankeet779@users.noreply.github.com>
2023-12-16 21:41:16 +0545

    EditProfile button for User

--- unify/frontend/src/components/Profile/UserProfile.jsx
+++ unify/frontend/src/components/Profile/UserProfile.jsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { FaUser } from "react-icons/fa";
-import { Grid, Box, Flex, Text } from "@chakra-ui/react";
+import { FaUser, FaTimes } from "react-icons/fa";
+import { Grid, Box, Flex, Text, Button, IconButton } from "@chakra-ui/react";
+import EditProfile from "./EditProfile";
 
 const UserProfile = () => {
   const [userData, setUserData] = useState(null);
+  const [showEditProfile, setShowEditProfile] = useState(false);
+
+  const handleEditProfileClick = () => {
+    setShowEditProfile(true);
+  };
+
+  const handleCloseEditProfile = () => {
+    setShowEditProfile(false);
+  };
 
   const fetchUserData = async () => {
     try {
@@ -20,67 +30,62 @@ const UserProfile = () => {
   }, []);
 
   return (
-    <Grid
-      className="Student"
-      p="3rem"
-      borderRadius="md"
-      justifyContent="center"
-    >
-      <Box className="gradient" borderRadius="md">
-        <Flex
-          className="profile_down"
-          alignItems="center"
-          justifyContent="center"
-        ></Flex>
-      </Box>
-      <Box
-        className="profile_title"
-        backgroundColor="gray.200"
-        borderRadius="md"
-      >
-        <Flex
-          flexDirection="column"
-          alignItems="center"
-          my="2rem"
-          maxHeight="100%"
-          maxW="18rem"
+    <Grid className="Student" p="3rem" borderRadius="md" justifyContent="center">
+      <Flex alignItems="flex-start">
+        <Box className="gradient" borderRadius="md">
+          <Flex className="profile_down" alignItems="center" justifyContent="center"></Flex>
+        </Box>
+        <Box
+          className="profile_title"
+          backgroundColor="gray.200"
+          borderRadius="md"
+          flex="0 0 65%"
+          mr="1rem"
+          p="1rem"
         >
-          <Box
-            bg="blue.400"
-            borderRadius="full"
-            p="1rem"
-            boxShadow="md"
-            mb="2rem"
-          >
-            <FaUser size={64} color="white" />
+          <Flex flexDirection="column" alignItems="center" my="1rem">
+            <Box bg="blue.400" borderRadius="full" p="1rem" boxShadow="md" mb="2rem">
+              <FaUser size={64} color="white" />
+            </Box>
+            <Text fontSize="large" fontWeight="600" color="gray.600" mt="-1.5rem">
+              <strong>{userData?.name} </strong>
+              Ankit Rimal
+            </Text>
+            <Button colorScheme="blue" onClick={handleEditProfileClick} mt="1rem">
+              Edit Profile
+            </Button>
+            <Text ml="2rem">
+              <Text fontSize="md" fontWeight="400" color="gray.600" mt=".5rem">
+                <strong>Department:</strong><br/> {userData?.department}
+                School of Computer Science and Engineering
+              </Text>
+              <Text fontSize="md" fontWeight="400" color="gray.600" mt=".5rem">
+                <strong>Country:</strong><br/> {userData?.country}
+                Nepal
+              </Text>
+              <Text fontSize="md" fontWeight="400" color="gray.600">
+                <strong>Email address:</strong>{" "}
+                <small>{userData?.email}</small>
+                ar41041720@student.ku.edu.np
+              </Text>
+            </Text>
+          </Flex>
+        </Box>
+        {showEditProfile && (
+          <Box className="edit_profile_section" bg="gray.100" borderRadius="md" p="1rem" my="-2.5rem">
+            <Flex flexDirection="column" alignItems="flex-end">
+              <IconButton
+                aria-label="Close"
+                icon={<FaTimes />}
+                colorScheme="red"
+                onClick={handleCloseEditProfile}
+                mb="1rem"
+              />
+              <EditProfile onClose={handleCloseEditProfile} />
+            </Flex>
           </Box>
-          <Text fontSize="large" fontWeight="600" color="gray.600" mt="-1.5rem">
-            <strong>{userData?.name}</strong>
-            Ankit Rimal
-          </Text>
-          <Text ml="2rem">
-            <Text fontSize="md" fontWeight="400" color="gray.600" mt=".5rem">
-              <strong>
-                Department: <br />{" "}
-              </strong>{" "}
-              {userData?.department}
-              School of Computer Science and Engineering
-            </Text>
-            <Text fontSize="md" fontWeight="400" color="gray.600" mt=".5rem">
-              <strong>
-                Country:
-                <br />{" "}
-              </strong>{" "}
-              {userData?.country}
-              Nepal
-            </Text>
-            <Text fontSize="md" fontWeight="400" color="gray.600">
-              <strong>Email address: </strong> <small>{userData?.email}</small>
-              ar41041720@student.ku.edu.np
-            </Text>
-          </Text>
-        </Flex>
-      </Box>
+        )}
+      </Flex>
     </Grid>
   );
 };
